92d35807fe601379644c1131a7e6001fc979ceb0
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-04-01 18:06:56 +0200

    responsive modifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5
+    sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex
-      flex-col gap-2">
+      flex-col md:flex-row gap-2">
         <input 
           className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none
           ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow
@@ -10,8 +11,8 @@ export default function Home() {
           type="text" 
           placeholder="Search here..." 
         />
-        <button className="bg-black bg-opacity-50 text-white font-medium py-2 rounded-full 
-        active:scale-90 focus:scale-90 transition-transform outline-none">
+        <button className="bg-black text-white font-medium py-2 rounded-full 
+        active:scale-90 transition-transform outline-none md:px-10">
           Search
         </button>
       </div>
