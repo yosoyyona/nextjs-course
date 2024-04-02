6b57457ee4bbbbf7d5df3831043c2e2583a982cc
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-04-02 16:10:12 +0200

    plugins - tailwindcss/forms

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5
     sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        <input />
         <a href="#">AAA</a>
         <button className="btn">Submit</button>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,8 @@ const config: Config = {
       }
     }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 };
 export default config;
