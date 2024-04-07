f237e4aa75c470c42a1bf7154e7dd0d46a1a4426
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-04-07 20:27:13 +0200

    Route Handlers

--- app/create-acount/page.tsx
+++ app/create-acount/page.tsx
@@ -1,13 +1,12 @@
-import Link from "next/link";
-import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import FormInput from "../components/form-input";
 import FormButton from "../components/form-btn";
+import SocialLogin from "../components/social-login";
 
 export default function CreateAccount() {
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl">안녕하세요!</h1>
+        <h1 className="text-2xl">Hey!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
       <form className="flex flex-col gap-3">
@@ -25,13 +24,7 @@ export default function CreateAccount() {
         />
         <FormButton loading={true} text="Create account" />
       </form>
-      <div className="w-full h-px bg-neutral-500" />
-      <div>
-        <Link className="primary-btn flex h-10 items-center justify-center gap-3" href="/sms">
-          <span><ChatBubbleOvalLeftEllipsisIcon className="h-6 w-6" /></span>
-          <span>Sign up with SMS</span>
-        </Link>
-      </div>
+      <SocialLogin />
     </div>
   );
 }
