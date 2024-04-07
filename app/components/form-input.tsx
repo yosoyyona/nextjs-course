9253b771158546dc0dc6b0dd059de42658f2e40e
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-04-07 20:43:07 +0200

    server action - FormData, use server

--- app/components/form-input.tsx
+++ app/components/form-input.tsx
@@ -3,15 +3,19 @@ interface FormInputProps {
   placeholder: string;
   required: boolean;
   errors: string[];
+  name: string;
 }
 
-export default function FormInput({type, placeholder, required, errors}: FormInputProps) {
+export default function FormInput({type, placeholder, required, errors, name}: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
-      <input className="bg-transparent rounded-md w-full h-10 focus:outline-none
+      <input name={name}
+        className="bg-transparent rounded-md w-full h-10 focus:outline-none
         ring-2 focus:ring-4 ring-neutral-200 focus:ring-orange-500 transition
         border-none placeholder:text-neutral-400" 
-        type={type} placeholder={placeholder} required={required} 
+        type={type} 
+        placeholder={placeholder} 
+        required={required} 
       />
       {errors.map((error, index) => (
         <span key={index} className="text-red-500 font-medium">
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,21 @@ import FormInput from "../components/form-input";
 import SocialLogin from "../components/social-login";
 
 export default function LogIn() {
+  const onSubmit = async (formData: FormData) => {
+    "use server";
+    console.log("I run in the server!")
+    console.log(formData.get("email"), formData.get("password")); // showed in the server!
+  }
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">Hey!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={onSubmit} className="flex flex-col gap-3">
+        <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
         <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
