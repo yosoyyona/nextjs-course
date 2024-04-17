2c3b7077941ca283df18909a517045af5ba67d95
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-04-17 21:29:10 +0200

    Zod - refine

--- app/create-acount/action.ts
+++ app/create-acount/action.ts
@@ -1,12 +1,24 @@
 "use server";
-import {z} from "zod";
+import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(5).max(10),
-  email: z.string().email(),
-  password: z.string().min(10).max(20),
-  confirmPassword: z.string().min(10).max(20),
-});
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string.",
+        required_error: "Where is my username?",
+      })
+      .min(5, "Way too short!")
+      .max(10, "Way too long!")
+      .refine((username) => !username.includes("patata"), "No patata!"),
+    email: z.string().email(),
+    password: z.string().min(10).max(20),
+    confirmPassword: z.string().min(10).max(20),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "Both passwords should be the same!",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -16,7 +28,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirmPassword: formData.get("confirmPassword"),
   };
   const result = formSchema.safeParse(data);
-  if(!result.success) {
+  if (!result.success) {
     return result.error.flatten();
   }
 }
