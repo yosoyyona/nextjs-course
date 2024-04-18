b69e45425de5818c0db69f95411641468d92c6fa
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-04-18 20:21:39 +0200

    SMS validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,11 +2,42 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine((phone) => validator.isMobilePhone(phone, "es-ES"), "Wrong Phone format");
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+interface ActionState {
+  token: boolean
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if(!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if(!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      }
+    } else {
+      return {
+        token: true,
+      }
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if(!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      }
+    } else {
+        redirect("/");
+    }
+  }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,13 @@ import Button from "../components/button";
 import Input from "../components/input";
 import { smsLogin } from "./actions";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogIn() {
-  const [state, action] = useFormState(smsLogin, null);
+  const [state, action] = useFormState(smsLogin, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -14,21 +19,25 @@ export default function SMSLogIn() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={action} className="flex flex-col gap-3">
-        <Input
-          name="phone"
-          type="text"
-          placeholder="Phone number"
-          required
-        />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="Verify" />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
