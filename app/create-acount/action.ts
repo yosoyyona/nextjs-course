6f5ee535db83ba4b92dc020fe1ef62b414e7e9b1
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-04-19 20:19:40 +0200

    db validation

--- app/create-acount/action.ts
+++ app/create-acount/action.ts
@@ -4,7 +4,43 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+
+const checkUsername = (username: string) => !username.includes("potato");
+const checkPasswords = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+// check if username is taken
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+// check if email is already used
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
 
 const formSchema = z
   .object({
@@ -15,16 +51,24 @@ const formSchema = z
       })
       .toLowerCase()
       .trim()
-      .transform((username) => `${username}🥕`)
-      .refine((username) => !username.includes("patata"), "No patata!"),
-    email: z.string().email().toLowerCase(),
+      //.transform((username) => `${username}🥕`)
+      .refine(checkUsername, "No potatoes allowed!")
+      .refine(checkUniqueUsername, "This username is already taken"),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .refine(
+        checkUniqueEmail,
+        "There is an acount already registered with this email"
+      ),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH)
       .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-      confirmPassword: z.string().min(PASSWORD_MIN_LENGTH),
+    confirmPassword: z.string().min(PASSWORD_MIN_LENGTH),
   })
-  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+  .refine(checkPasswords, {
     message: "Both passwords should be the same!",
     path: ["confirmPassword"],
   });
@@ -36,10 +80,13 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirmPassword: formData.get("confirmPassword"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // hash password
+    // save the user to db
+    // log the user in
+    // redirect "/home"
   }
 }
--- lib/db.ts
+++ lib/db.ts
@@ -2,21 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.create({
-    data: {
-      token: "1212",
-      user: {
-        connect: {
-          id: 1,
-        }
-      }
-    }
-  })
-  console.log(token);
-}
-
-test();
-
-
 export default db;
